22e1034301a67fd1936d2cca1e2d617d2e72c448
Lubos Krnac <lubos.krnac@gmail.com>
2024-04-17 04:36:40 +0200

    Rename folder

--- product-service/src/app.module.ts
+++ product-service/src/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { Product } from "./product/products.entity";
-import { ProductsModule } from "./product/products.module";
+import { Product } from "./products/products.entity";
+import { ProductsModule } from "./products/products.module";
 
 @Module({
   imports: [
